a79ce83de3d6176ea471f5659fadb1d2d6e1b88a
Mohamed Nidal <touhami9@gmail.com>
2023-08-14 20:55:29 +0100

    building the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
-import Home from './pages/Home';
+import Homepage from './pages/Homepage';
 import Pricing from './pages/Pricing';
 import NotFound from './pages/NotFound';
+import Login from './pages/Login';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,9 +12,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './PageNav.module.css';
+import Logo from './Logo';
 
 export default function PageNav() {
   return (
-    <ul>
-      <li>
-        <NavLink to={'/'}>Home</NavLink>
-      </li>
+    <nav className={styles.nav}>
+      <Logo />
+      <ul>
+        <li>
+          <NavLink to={'/product'}>Product</NavLink>
+        </li>
 
-      <li>
-        <NavLink to={'/product'}>Product</NavLink>
-      </li>
+        <li>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
 
-      <li>
-        <NavLink to={'/pricing'}>Pricing</NavLink>
-      </li>
-    </ul>
+        <li>
+          <NavLink to={'/login'} className={styles.ctaLink}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
