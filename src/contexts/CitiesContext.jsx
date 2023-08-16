1a35e40e75a78904f26cf9aad3dd1d52ab319988
Mohamed Nidal <touhami9@gmail.com>
2023-08-16 18:27:48 +0100

    creating a cities context

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+
+const BASE_URL = 'http://localhost:8000';
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+
+    // return () => {
+    //   second
+    // }
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+export { CitiesProvider };
